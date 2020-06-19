e73e8aa8bc21ed60325a2dc0479905fb92685a7b
Ivanildo Lopes da Silva Junior <ivanildo.lopes.jr@gmail.com>
2020-06-19 17:32:36 -0300

    Ajustando o sistema

--- src/main/resources/db/migration/V002__cria-tabela-items.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE items (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    point_id BIGINT NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    
-    PRIMARY KEY (id)
-);
--- src/main/resources/db/migration/V003__cria-relacionamento-tabelas.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE items ADD CONSTRAINT fk_points
-FOREIGN KEY (point_id) REFERENCES points (id);
