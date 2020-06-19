66d8e0ccb3cf1bc93ffe21d0fee814b5f6e59f20
Ivanildo Lopes da Silva Junior <ivanildo.lopes.jr@gmail.com>
2020-06-19 10:06:57 -0300

    Iniciando o projeto

---
+++ src/main/java/cv/justa/desafio/domain/model/Points.java
@@ -0,0 +1,103 @@
+package cv.justa.desafio.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Points {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String whatsapp;
+	
+	private Double latitude;
+	
+	private Double longitude;
+	
+	private String city;
+	
+	private String uf;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getWhatsapp() {
+		return whatsapp;
+	}
+	public void setWhatsapp(String whatsapp) {
+		this.whatsapp = whatsapp;
+	}
+	public Double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+	public Double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getUf() {
+		return uf;
+	}
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Points other = (Points) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+		
+}
---
+++ src/main/java/cv/justa/desafio/domain/repository/PointsRepository.java
@@ -0,0 +1,11 @@
+package cv.justa.desafio.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import cv.justa.desafio.domain.model.Points;
+
+@Repository
+public interface PointsRepository extends JpaRepository<Points, Long>{
+
+}
